aae9e8a4376d7aa5c0ca214ffc5de0c120378ec5
VictorTPhan <71913548+VictorTPhan@users.noreply.github.com>
2024-01-12 10:24:59 -0800

    Update masterlist.py

--- masterlist.py
+++ masterlist.py
@@ -14,22 +14,22 @@ MASTER_LIST = [
     "Miniature concerto",
     "Alec Rowley",
     Mode(
-      "https://emma-server.onrender.com/miniature_concerto/primo_pdf",
-      "https://emma-server.onrender.com/miniature_concerto/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/primo_pdf.mp3",
       [
         24.11, 58.44, 63.54, 91.15, 113.06, 140.96, 163.59, 190.49, 219.76,
         250.07
       ], [1, 1, 1, 1, 1, 1, 1, 1]),
     Mode(
-      "https://emma-server.onrender.com/miniature_concerto/secondo_pdf",
-      "https://emma-server.onrender.com/miniature_concerto/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/secondo_pdf.mp3",
       [
         24.11, 58.44, 63.54, 91.15, 113.06, 140.96, 163.59, 190.49, 219.76,
         250.07
       ], [1, 1, 1, 1, 1, 1, 1, 1]),
     Mode(
-      "https://emma-server.onrender.com/miniature_concerto/both_pdf",
-      "https://emma-server.onrender.com/miniature_concerto/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/miniature_concerto/both_pdf.mp3",
       [
         24.11, 58.44, 63.54, 91.15, 113.06, 140.96, 163.59, 190.49, 219.76,
         250.07
@@ -38,111 +38,111 @@ MASTER_LIST = [
   Song(
     "Prelude Defaun", "Debussy",
     Mode(
-      "https://emma-server.onrender.com/prelude_defaun/primo_pdf",
-      "https://emma-server.onrender.com/prelude_defaun/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/primo_pdf.mp3",
       [37.0, 53.0, 37.0, 57.0, 66.0], [1, 1, 1, 1]),
     Mode(
-      "https://emma-server.onrender.com/prelude_defaun/secondo_pdf",
-      "https://emma-server.onrender.com/prelude_defaun/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/secondo_pdf.mp3",
       [37.0, 53.0, 37.0, 57.0, 66.0], [1, 1, 1, 1]),
     Mode(
-      "https://emma-server.onrender.com/prelude_defaun/both_pdf",
-      "https://emma-server.onrender.com/prelude_defaun/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/prelude_defaun/both_pdf.mp3",
       [37.0, 53.0, 37.0, 57.0, 66.0], [1, 1, 1, 1])),
   Song(
     "Moderato", "Cornelius Gurlitt",
     Mode(
-      "https://emma-server.onrender.com/moderato/primo_pdf",
-      "https://emma-server.onrender.com/moderato/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/primo_pdf.mp3",
       [], []),
     Mode(
-      "https://emma-server.onrender.com/moderato/secondo_pdf",
-      "https://emma-server.onrender.com/moderato/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/secondo_pdf.mp3",
       [], []),
     Mode(
-      "https://emma-server.onrender.com/moderato/both_pdf",
-      "https://emma-server.onrender.com/moderato/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/moderato/both_pdf.mp3",
       [], [])),
   Song(
     "Rondino", "Joseph Hadyn",
     Mode(
-      "https://emma-server.onrender.com/rondino/primo_pdf",
-      "https://emma-server.onrender.com/rondino/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/primo_pdf.mp3",
       [23.69, 35.66], [1, -1]),
     Mode(
-      "https://emma-server.onrender.com/rondino/secondo_pdf",
-      "https://emma-server.onrender.com/rondino/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/secondo_pdf.mp3",
       [23.69, 35.66], [1, -1]),
     Mode(
-      "https://emma-server.onrender.com/rondino/both_pdf",
-      "https://emma-server.onrender.com/rondino/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/rondino/both_pdf.mp3",
       [23.69, 35.66], [1, -1])),
   Song(
     "Menuet", "Amadeus Mozart",
     Mode(
-      "https://emma-server.onrender.com/menuet/primo_pdf",
-      "https://emma-server.onrender.com/menuet/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/primo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/menuet/secondo_pdf",
-      "https://emma-server.onrender.com/menuet/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/secondo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/menuet/both_pdf",
-      "https://emma-server.onrender.com/menuet/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/menuet/both_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5])),
   Song(
     "Concert", "Amadeus Mozart",
     Mode(
-      "https://emma-server.onrender.com/concert/primo_pdf",
-      "https://emma-server.onrender.com/concert/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/primo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/concert/secondo_pdf",
-      "https://emma-server.onrender.com/concert/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/secondo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/concert/both_pdf",
-      "https://emma-server.onrender.com/concert/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concert/both_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.71, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5])),
   Song(
     "Concerto", "Billy Joel",
     Mode(
-      "https://emma-server.onrender.com/concerto/primo_pdf",
-      "https://emma-server.onrender.com/concerto/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/primo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/concerto/secondo_pdf",
-      "https://emma-server.onrender.com/concerto/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/secondo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/concerto/both_pdf",
-      "https://emma-server.onrender.com/concerto/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/concerto/both_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5])),
   Song(
     "Easy piano duet", "Lopez Calvo Gabriel Alejadro",
     Mode(
-      "https://emma-server.onrender.com/easy_piano_duet/primo_pdf",
-      "https://emma-server.onrender.com/easy_piano_duet/primo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/primo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/primo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/easy_piano_duet/secondo_pdf",
-      "https://emma-server.onrender.com/easy_piano_duet/secondo_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/secondo_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/secondo_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]),
     Mode(
-      "https://emma-server.onrender.com/easy_piano_duet/both_pdf",
-      "https://emma-server.onrender.com/easy_piano_duet/both_audio",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/both_audio.mp3",
+      "https://github.com/VictorTPhan/MasterSongList/raw/master/easy_piano_duet/both_pdf.mp3",
       [46.97, 102.36, 135.04, 152.83, 165.61, 184.98, 208.62],
       [1, 1, 1, -1, 1, -3, 5]))
 ]
